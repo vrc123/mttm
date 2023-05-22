20fd68d960072cb8a353efe18fd7864b5e06ac0f
Victor Rosager Christensen <cph-vc48@cphbusiness.dk>
2023-05-22 19:28:39 +0200

    edited

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,72 +1,61 @@
 describe('Test of MTTM', () => {
-  it('Creates an anlysis', () => {
+
+  before(() => {
     cy.visit('http://localhost:3000/')
+  })
+
+  after(() => {
+    cy.visit('http://localhost:3000/my-opponents')
+
+    cy.get('.OpponentList_opponentList__xp7Ew').contains('.OpponentItem_opponentItem__8xMoL', 'Test Test Test').within(() => {
+      cy.get('button').contains('Delete').click()
+    })
+    cy.get('button').contains('Yes').click()
+  })
+
+  it('Creates an analysis', () => {
 
     cy.get('h1').should('have.text', 'My table tennis matches')
     cy.get('button').contains('Create an analysis').click()
 
     cy.get('h1').should('have.text', 'Create an analysis')
-    cy.get('input').its('length').should('eq', 1)
     cy.get('input').eq(0).type('2000-10-10')
+    cy.get('input').eq(0).should('have.value', '2000-10-10')
     cy.get('button').eq(0).click()
 
     cy.get('h1').should('have.text', 'Select your opponent')
     cy.get('a').contains('Click here').click()
     
     cy.get('h1').should('have.text', 'Add and select opponent')
-    cy.get('input').its('length').should('eq', 5)
     cy.get('input').eq(0).type('Test')
+    cy.get('input').eq(0).should('have.value', 'Test')
     cy.get('input').eq(1).type('Test')
+    cy.get('input').eq(1).should('have.value', 'Test')
     cy.get('input').eq(2).type('Test')
+    cy.get('input').eq(2).should('have.value', 'Test')
     cy.get('input').eq(3).type('Test')
+    cy.get('input').eq(3).should('have.value', 'Test')
     cy.get('input').eq(4).type('2000-10-10')
+    cy.get('input').eq(4).should('have.value', '2000-10-10')
     cy.get('button').contains('Add opponent').click()
     
     cy.get('h1').should('have.text', 'Add the score of the match')
-    cy.get('input').its('length').should('eq', 2)
     cy.get('input').eq(0).type('4')
+    cy.get('input').eq(0).should('have.value', '4')
     cy.get('input').eq(1).type('3')
+    cy.get('input').eq(1).should('have.value', '3')
     cy.get('button').contains('Next').click()
 
     cy.get('h1').should('have.text', 'Add serves')
-    cy.get('input').its('length').should('eq', 2)
     cy.get('button').contains('Next').click()
 
     cy.get('h1').should('have.text', 'Add recieves')
-    cy.get('input').its('length').should('eq', 2)
     cy.get('button').contains('Next').click()
 
     cy.get('h1').should('have.text', 'Add match rallies')
-    cy.get('input').its('length').should('eq', 2)
     cy.get('button').contains('Create analysis').click()
-    cy.get('.AnalysisItem_analysisItem__UCTCo')
-  })
-
-  /*
-
-
-
-  it('passess', () => {
-    cy.visit('http://localhost:3000/my-opponents')
-  })
-  it('passess', () => {
-    cy.visit('http://localhost:3000/')
-    
-    cy.get('h1').should('have.text', 'My table tennis matches')
-    cy.get('button').contains('My analyses').click()
 
     cy.get('h1').should('have.text', 'My analyses')
-    cy.get('.AnalysisItem_analysisItem__UCTCo').within(() => {})
-    //.contains('p', 'Test')
-  })*/
-  /*
-  it('passes', () => {
-    cy.visit('http://localhost:3000/')
-
-    cy.get('h1').should('have.text', 'My table tennis matches')
-    cy.get('nav').contains('a', 'My opponents').click()
-    
-    cy.get('h1').should('have.text', 'My opponents')
-    cy.get('.OpponentItem_opponentItem__8xMoL').contains('p', 'Test')
-  })*/
+    cy.get('.AnalysisItem_analysisItem__UCTCo').last().should('contain', 'Test Test Test')
+  })
 })
