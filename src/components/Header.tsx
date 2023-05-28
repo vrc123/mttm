2247abd9d9f64ab848ddb77431b4e7d378e8934c
Victor Rosager Christensen <cph-vc48@cphbusiness.dk>
2023-05-29 00:05:01 +0200

    edited

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import styles from '@/styles/components/Header.module.css'
 import Link from 'next/link';
 import NavLink from './NavLink';
+import { useEffect, useState } from 'react';
 
 const navLinks = [
   {
@@ -24,18 +25,52 @@ const navLinks = [
 
 export default function Header() {
 
+  const [showNavLinks, setShowNavLinks] = useState(false)
+  const [showIcon, setShowIcon] = useState(true)
+  const [switchIcon, setSwitchIcon] = useState(false)
+
+  useEffect(() => {
+    
+    if (screen.width <= 1024) {
+      setShowNavLinks(true)
+      setShowIcon(false)
+    }
+
+  }, [])
+
+
+  function showLinks() {
+    if (showNavLinks) {
+      setShowNavLinks(false)
+      setSwitchIcon(true)
+    } else {
+      setShowNavLinks(true)
+      setSwitchIcon(false)
+    }
+  }
+
   return (
     <header className={styles.header}>
-      <Link href='/'>
-        <h3>MTTM</h3>
-      </Link>
-      <nav>
+      <div className={`${!showIcon ? styles.menu : ''}`}>
+        <Link href='/'>
+          <h3>MTTM</h3>
+        </Link>
+        {!showIcon && <div className={styles.menuIcon} onClick={showLinks}>
+          {!switchIcon && <div className={styles.menuIconOpen}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>}
+          {switchIcon && <div className={styles.menuIconClose}></div>}
+        </div>}
+      </div>
+      {!showNavLinks && <nav>
         {navLinks.map((navLink) => {
           return (
             <NavLink key={navLink.id} {...navLink} />
           )
         })}
-      </nav>
+      </nav>}
     </header>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 import { Provider } from 'react-redux'
 import { store } from '../../store'
 import type { AppProps } from 'next/app'
@@ -6,9 +7,14 @@ import Header from '@/components/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Header />
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Provider store={store}>
+        <Header />
+        <Component {...pageProps} />
+      </Provider>
+    </>
   )
 }
